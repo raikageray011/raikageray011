a22fee48919764178c29df4e68453e4737d6a893
raikageray011 <100453983+raikageray011@users.noreply.github.com>
2022-10-02 13:22:07 -0400

    Fizz Buzz
    Just a little something I made...

---
+++ fizz_buzz.py
@@ -0,0 +1,13 @@
+integer = int(input())
+# TODO: get an integer from the user
+if (integer%3==0) and (integer%5==0):
+    print("fizzbuzz")
+elif(integer%3==0):
+    print("fizz")
+elif(integer%5==0):
+    print("buzz")
+else:
+    print("Neither fizz nor buzz nor fizzbuzz")
+
+
+# TODO: print out fizz and buzz based on the instructions
